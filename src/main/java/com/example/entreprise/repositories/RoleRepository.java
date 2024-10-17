1b9fd7481f76887b91b79041199e3ace68eb3b17
Anas <anas.rebai@esprit.tn>
2024-10-17 22:54:17 +0100

    first commit

--- src/main/java/com/example/entreprise/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.entreprise.repositories;
-
-import com.example.entreprise.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name); // Méthode pour trouver un rôle par son nom
-}
