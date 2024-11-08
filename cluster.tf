bf0077c6a0b6564c76141010683a820a3d91a67b
Anas <anas.rebai@esprit.tn>
2024-11-08 18:29:19 +0100

    update JenkinsFile

--- cluster.tf
+++ cluster.tf
@@ -5,19 +5,19 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "myterraformgroup" {
   name     = "myResourceGroup"
-  location = "East US"  # ou "eastus"
+  location = "East US"
 }
 
 resource "azurerm_kubernetes_cluster" "myakscluster" {
   name                = "myAKSCluster"
-  location            = azurerm_resource_group.myterraformgroup.location  # utilisation correcte
+  location            = azurerm_resource_group.myterraformgroup.location
   resource_group_name = azurerm_resource_group.myterraformgroup.name
   dns_prefix          = "myaksdns"
 
   default_node_pool {
     name            = "default"
     node_count      = 2
-    vm_size         = "Standard_B1s"  # Correction de la taille du VM
+    vm_size         = "Standard_B2s"  # Increased VM size to meet the requirements
     os_disk_size_gb = 30
   }
 
